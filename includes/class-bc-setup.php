cb16afb302e87dd680ef84b017277fc4c404b60a
Tyler Bailey <tyler.bailey@10up.com>
2021-02-24 07:28:14 -0500

    Switch isset() with empty()

--- includes/class-bc-setup.php
+++ includes/class-bc-setup.php
@@ -279,8 +279,8 @@ class BC_Setup {
 		$params['mimeTypes'] = BC_Utility::get_all_brightcove_mimetypes();
 
 		$defaultAccount             = $bc_accounts->get_account_details_for_user();
-		$params['defaultAccount']   = isset( $defaultAccount['hash'] ) ? $defaultAccount['hash'] : null;
-		$params['defaultAccountId'] = isset( $defaultAccount['account_id'] ) ? $defaultAccount['account_id'] : null;
+		$params['defaultAccount']   = ! empty( $defaultAccount['hash'] ) ? $defaultAccount['hash'] : null;
+		$params['defaultAccountId'] = ! empty( $defaultAccount['account_id'] ) ? $defaultAccount['account_id'] : null;
 
 		return $params;
 
